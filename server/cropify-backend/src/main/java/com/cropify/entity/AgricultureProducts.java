5a51b11af818f5d3310a13b2ee0e7cbdf8f72368
74635deepak <deepaknikhare30@gmail.com>
2024-02-06 17:08:27 +0530

    created getters and setters for all entities

--- server/cropify-backend/src/main/java/com/cropify/entity/AgricultureProducts.java
+++ server/cropify-backend/src/main/java/com/cropify/entity/AgricultureProducts.java
@@ -19,4 +19,34 @@ public class AgricultureProducts {
 	@Column(name = "agri_prod_type")
 	@Enumerated(EnumType.STRING)
 	private AgriProductType agriProductType;
+	
+	//-----------------------------Getter and Setters--------------------
+
+	public String getAgriProductId() {
+		return agriProductId;
+	}
+
+	public void setAgriProductId(String agriProductId) {
+		this.agriProductId = agriProductId;
+	}
+
+	public String getAgriProductName() {
+		return agriProductName;
+	}
+
+	public void setAgriProductName(String agriProductName) {
+		this.agriProductName = agriProductName;
+	}
+
+	public AgriProductType getAgriProductType() {
+		return agriProductType;
+	}
+
+	public void setAgriProductType(AgriProductType agriProductType) {
+		this.agriProductType = agriProductType;
+	}
+	
+	
+	
+	
 }
--- server/cropify-backend/src/main/java/com/cropify/entity/OrderMachineDetails.java
+++ server/cropify-backend/src/main/java/com/cropify/entity/OrderMachineDetails.java
@@ -35,4 +35,72 @@ public class OrderMachineDetails extends OrderBaseClass{
 	
 	@Column(name = "total_amount")
 	private double totalPrice;
+	
+	//-----------------------------Getter and Setters--------------------
+
+	public UserDetails getFarmerId() {
+		return farmerId;
+	}
+
+	public void setFarmerId(UserDetails farmerId) {
+		this.farmerId = farmerId;
+	}
+
+	public UserDetails getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(UserDetails sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	public Machinery getMachineId() {
+		return machineId;
+	}
+
+	public void setMachineId(Machinery machineId) {
+		this.machineId = machineId;
+	}
+
+	public int getRentDuration() {
+		return rentDuration;
+	}
+
+	public void setRentDuration(int rentDuration) {
+		this.rentDuration = rentDuration;
+	}
+
+	public LocalDate getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(LocalDate orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public LocalDate getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(LocalDate deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	
+	
 }
