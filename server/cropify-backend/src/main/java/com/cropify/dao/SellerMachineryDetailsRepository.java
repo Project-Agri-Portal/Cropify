892f2d1a15f3ba963515fc8911bdba3500cca1f4
Janak Khorgade <janak24fop@gmail.com>
2024-02-07 16:48:04 +0530

    fixed a derived query error

--- server/cropify-backend/src/main/java/com/cropify/dao/SellerMachineryDetailsRepository.java
+++ server/cropify-backend/src/main/java/com/cropify/dao/SellerMachineryDetailsRepository.java
@@ -4,18 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.cropify.entity.SellerMachineryDetails;
 
 public interface SellerMachineryDetailsRepository extends JpaRepository<SellerMachineryDetails, Long>{
 	// --- Find entity methods ---
 	List<SellerMachineryDetails> findAll();
-	
-	@Query	// Providing @Query annotation to fix the IDE error of 'no derived query'
-	Optional<SellerMachineryDetails> findById(Long sellerMachineryId);
+	Optional<SellerMachineryDetails> findBySellerMachineryId(Long sellerMachineryId);
 	
 	// --- Delete entity by ID ---
-	@Query
-	void deleteById(Long sellerMachineryId);
+	void deleteBySellerMachineryId(Long sellerMachineryId);
 }
