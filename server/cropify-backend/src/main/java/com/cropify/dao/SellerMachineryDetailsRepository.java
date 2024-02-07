fc37303fc79f34c67681e6c7dc7dafa95430cbed
Janak Khorgade <janak24fop@gmail.com>
2024-02-07 16:35:54 +0530

    declared 2 repository file methods

--- server/cropify-backend/src/main/java/com/cropify/dao/SellerMachineryDetailsRepository.java
+++ server/cropify-backend/src/main/java/com/cropify/dao/SellerMachineryDetailsRepository.java
@@ -1,5 +1,21 @@
 package com.cropify.dao;
 
-public interface SellerMachineryDetailsRepository {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.cropify.entity.SellerMachineryDetails;
+
+public interface SellerMachineryDetailsRepository extends JpaRepository<SellerMachineryDetails, Long>{
+	// --- Find entity methods ---
+	List<SellerMachineryDetails> findAll();
+	
+	@Query	// Providing @Query annotation to fix the IDE error of 'no derived query'
+	Optional<SellerMachineryDetails> findById(Long sellerMachineryId);
+	
+	// --- Delete entity by ID ---
+	@Query
+	void deleteById(Long sellerMachineryId);
 }
--- server/cropify-backend/src/main/java/com/cropify/dao/UserDetailsRepository.java
+++ server/cropify-backend/src/main/java/com/cropify/dao/UserDetailsRepository.java
@@ -1,5 +1,18 @@
 package com.cropify.dao;
 
-public interface UserDetailsRepository{
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cropify.entity.UserDetails;
+
+public interface UserDetailsRepository extends JpaRepository<UserDetails, Long>{
+	// --- Find user methods ---
+	List<UserDetails> findAll();
+	List<UserDetails> findAllById(Long userId);
+	Optional<UserDetails> findById(Long userId);
 	
+	// --- Delete a user by ID ---
+	void deleteById(Long userId);
 }
