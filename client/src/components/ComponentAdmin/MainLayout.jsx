fca037a0487387326de6eefb5c28fd66435386a3
tejas-mohitkar <tejasmohitkar3@gmail.com>
2024-02-10 20:24:15 +0530

    created admin component

---
+++ client/src/components/ComponentAdmin/MainLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
+import Header from './Header'
+import Sidebar from './SideBar'
+import Home from './Home'
+import "./MainLayout.css";
+
+function MainLayout() {
+    
+
+}
+
+export default MainLayout
