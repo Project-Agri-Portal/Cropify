eac80244806da565bff64dfcec75963da7fd1338
tejas-mohitkar <tejasmohitkar3@gmail.com>
2024-02-10 20:29:03 +0530

    created admin sidebar

--- client/src/components/ComponentAdmin/MainLayout.jsx
+++ client/src/components/ComponentAdmin/MainLayout.jsx
@@ -7,7 +7,19 @@ import "./MainLayout.css";
 
 function MainLayout() {
     
+    const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
 
+    const OpenSidebar = () => {
+      setOpenSidebarToggle(!openSidebarToggle)
+    }
+  
+    return (
+      <div className='grid-container'>
+        <Header OpenSidebar={OpenSidebar}/>
+        <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+        <Home />
+      </div>
+    )
 }
 
 export default MainLayout
