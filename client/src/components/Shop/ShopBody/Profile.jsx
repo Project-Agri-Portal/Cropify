4889b351ff881e9cbdc710fa13caad2fe0bb4903
tejas-mohitkar <tejasmohitkar3@gmail.com>
2024-02-11 09:56:23 +0530

    Added User profile

---
+++ client/src/components/Shop/ShopBody/Profile.jsx
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react'
+import {Link} from "react-router-dom"
+import {useSelector} from "react-redux";
+// import MetaData
+
+const Profile = () => {
+
+    const {userEvent, loading} = useSelector(state => state.auth);
+
+  return (
+    <Fragment>
+        {loading ? }
+    </Fragment>
+  )
+}
+
+export default Profile
