bb6ad0ec3446fe9c5af6b8756c920d9011b02835
Janak Khorgade <janak24fop@gmail.com>
2024-02-17 12:08:18 +0530

    Automatic login and clearing of credentials on logout

--- client/src/components/ComponentsSeller/SellerProfile.jsx
+++ client/src/components/ComponentsSeller/SellerProfile.jsx
@@ -1,8 +1,18 @@
-import { Link } from "react-router-dom/cjs/react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 // import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./SellerProfile.css";
 
 function SellerProfile() {
+
+  const history = useHistory();
+
+  // Loggin out user and clearing the storages
+  function logoutUser() {
+    localStorage.clear();
+    sessionStorage.clear();
+    history.replace("/");
+  }
+
   return (
     <>
       <div className="d-flex" id="wrapper">
@@ -65,12 +75,14 @@ function SellerProfile() {
             >
               <i className="fas fa-map-marker-alt me-2"></i>Outlet
             </Link>
-            <Link
-              to="/"
+            <p
+              // to="/"
+              onClick={logoutUser}
+              style={{ cursor:"pointer" }}
               className="list-group-item list-group-item-action bg-transparent text-danger fw-bold"
             >
               <i className="fas fa-power-off me-2"></i>Logout
-            </Link>
+            </p>
           </div>
         </div>
 
@@ -182,7 +194,7 @@ function SellerProfile() {
 
                   <div
                     className="card mt-3"
-                    style={{ "background-color": "burlywood;" }}
+                    style={{ backgroundColor: "burlywood" }}
                   >
                     <ul className="list-group list-group-flush">
                       <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
@@ -194,9 +206,9 @@ function SellerProfile() {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             className="feather feather-globe mr-2 icon-inline"
                           >
                             <circle cx="12" cy="12" r="10"></circle>
@@ -218,9 +230,9 @@ function SellerProfile() {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             className="feather feather-github mr-2 icon-inline"
                           >
                             <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
@@ -238,9 +250,9 @@ function SellerProfile() {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             className="feather feather-twitter mr-2 icon-inline text-info"
                           >
                             <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
@@ -258,9 +270,9 @@ function SellerProfile() {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             className="feather feather-instagram mr-2 icon-inline text-danger"
                           >
                             <rect
@@ -287,9 +299,9 @@ function SellerProfile() {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             className="feather feather-facebook mr-2 icon-inline text-primary"
                           >
                             <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
