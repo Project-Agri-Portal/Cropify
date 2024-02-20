97c8905c98561bc05f41f040c6148807d7a7884c
Janak Khorgade <janak24fop@gmail.com>
2024-02-21 00:45:46 +0530

    integrated RazorPay API for customer orders

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
 import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
-// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-// import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
 import Login from "./components/UserAuthentication/SignIn/Login";
 import Register from "./components/UserAuthentication/SignUp/Register";
 import CommonHome from "./components/HomePage/CommonHome";
 import Shop from "./components/Shop/shop.jsx";
 import BlogPage from "./components/ComponentsFarmer/FarmerBlog/BlogPage";
-// import FarmerProfile from "./components/ComponentsFarmer/FarmerProfile/FarmerProfile";
-// import FarmerCrop from "./components/ComponentsFarmer/FarmerCrops/FarmerCrop";
 import Contact from "./components/Shop/ShopBody/Contact.jsx";
 import SellerMainPage from "./components/ComponentsSeller/SellerMainPage.jsx";
-// import Seller from "./components/ComponentsSeller/Dashboard.jsx";
-// import SellerProfile from "./components/ComponentsSeller/SellerProfile.jsx";
-// import OrderList from "./components/ComponentsSeller/SellerOrderList.jsx";
-// import ProductList from "./components/ComponentsSeller/SellerProducts.jsx";
-// import AddProductList from "./components/ComponentsSeller/AddProduct.jsx";
-// import SellerSidebar from "./components/ComponentsSeller/sidebar.jsx";
 import PageNotFound from "./components/NotFound/PageNotFound.jsx";
 import ShopAboutUs from "./components/Shop/ShopBody/AboutUs.jsx"
 import ProductListAll from "./components/Shop/ShopBody/ProductList.jsx"
@@ -32,7 +22,6 @@ import MachineryPage from "./components/InformationPage/MachineryPage.jsx";
 import Orders from "./components/Shop/ShopBody/Orders.jsx";
 import AddProduct from "./components/ComponentAdmin/AddProduct.jsx";
 import UserProfile from "./components/Shop/ShopBody/UserProfile.jsx";
-// import WeatherAPI from "./components/InformationPage/WeatherAPI.jsx";
 import FarmerMainPage from "./components/ComponentsFarmer/FarmerMainPage.jsx";
 
 function App() {
@@ -48,18 +37,9 @@ function App() {
           <Route path="/home/information/machinery" component={MachineryPage} exact />
           <Route path="/home/customer" component={Shop} exact />
           <Route path="/home/farmer" component={FarmerMainPage} exact />
-          {/* <Route path="/home/farmer/weather" component={WeatherAPI} exact /> */}
-          {/* <Route path="/home/seller" component={SellerProfile} exact /> */}
           <Route path="/home/seller" component={SellerMainPage} exact />
           <Route path="/blog" component={BlogPage} exact />
           <Route path="/contact" component={Contact} exact />
-          {/* <Route path="/farmer/profile" component={FarmerProfile} exact />
-          <Route path="/farmer/cropinfo" component={FarmerCrop} exact /> */}
-          {/* <Route path="/seller/profile" component={SellerProfile} exact /> */}
-          {/* <Route path="/seller/dashboard" component={Seller} exact />
-          <Route path="/seller/orderlist" component={OrderList} exact />
-          <Route path="/seller/productlist" component={ProductList} exact />
-          <Route path="/seller/addproduct" component={AddProductList} exact /> */}
 
           <Route path="/shop/about" component={ShopAboutUs} exact />
           <Route path="/shop/productsall" component={ProductListAll} exact />
@@ -75,11 +55,6 @@ function App() {
           <Route path="/admin/farmer" component={AdminFarmer} exact />
           <Route path="/admin/addproduct" component={AddProduct} exact />
 
-
-          {/* <Route path="/shop/productdetails" component={SingleProductDetails} exact /> */}
-          
-          {/* <Route path="/shop/home" component={Shop} exact /> */}
-          {/* <Route path="/shop" component={Shop} exact /> */}
           <Route path="*" component={PageNotFound} />
         </Switch>
       </div>
